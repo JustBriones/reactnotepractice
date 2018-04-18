8e67b99422240c9983d5a818d10486faa72191ac
JustBriones <justbriones@gmail.com>
2018-04-18 13:15:29 -0700

    added to the todolist component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
  
 class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e) {
+
+  }
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task">
+          <form onSubmit={this.addItem}>
+            <input placeholder="enter something">
             </input>
             <button type="submit">add</button>
           </form>
